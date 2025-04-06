22331ef99d0d9c392cc669e165c52793fff646b5
ratulislam46 <arfanratul46@gmail.com>
2025-04-06 16:58:17 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+import './App.css'
+import Blogs from './component/Blogs/Blogs'
+import Navbar from './component/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <>
+
+      <Navbar></Navbar>
+      
+      <div className="main-container flex text-center">
+        <div className="left-container w-[70%]">
+        <Blogs></Blogs>
+        </div>
+
+        <div className="right-container w-[30%]">
+          <h1>Reading time : 0 </h1>
+          <h1>Bookmarked Count : 0</h1>
+        </div>
+        
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Blogs/Blogs.jsx
@@ -0,0 +1,32 @@
+
+import React, { useEffect, useState } from 'react';
+import Blog from '../Blog/Blog';
+
+const Blogs = () => {
+
+const [blogs, setBlogs] = useState([])  
+    
+    useEffect(() => {
+        fetch("blogs.json")
+        .then(res => res.json())
+        .then(data=>setBlogs(data))
+    }, []);
+
+    // console.log(blogs);
+
+    return (
+        <div>
+            <h1 className='text-3xl'>Total : {blogs.length}</h1>
+
+            <div className="all-blogs grid grid-cols-2 gap-5">
+
+                {
+                    blogs.map((blog) =><Blog key={blog.id} blog={blog}></Blog>)
+                }
+            </div>
+        </div>
+    )
+};
+
+
+export default Blogs;
